86dd09cfa5a401935d16ec7f13af5246580c3173
HwiRwu <winter1152@uos.ac.kr>
2024-03-01 11:50:31 +0900

    1.2 Made real two variable function.

--- function.h
+++ function.h
@@ -5,12 +5,12 @@
 const double pi = 3.14159265358979;
 
 
-double one_variable_function(double x, double y, bool complex_type) {
+double one_variable_function_complex(double x, double y, bool complex_type) {
     std::complex<double> z(x, y);
+    std::complex<double> c(-1, 3);
 
     //Write one-varable complex function.
-    std::complex<double> function_value = exp(z);
-
+    std::complex<double> function_value = z / 3.0;
     double result;
     if (complex_type == true)
         result = function_value.real();
@@ -19,6 +19,13 @@ double one_variable_function(double x, double y, bool complex_type) {
 
     return result;
 }
+
+double two_variable_real_function(double x, double y) {
+    double z = x*y;
+
+    return z;
+}
+
 
 void origin_function_one(sf::RenderWindow& window, sf::VertexArray& graph, double size, double one_variable_input_function(double, double, bool), double start, double end, double x_angle, double y_angle, bool complex_type) {
     sf::Color transparentRed = sf::Color(0, 0, 255, 100);
@@ -32,7 +39,7 @@ void origin_function_one(sf::RenderWindow& window, sf::VertexArray& graph, doubl
 
             // Adjust the transformed coordinates to fit with the axes
             double x_final = x * std::sin(theta) + y * std::cos(theta);
-            double y_final = z * std::cos(psy) + (x * std::cos(theta) - y * std::sin(theta))* std::sin(psy);
+            double y_final = (x * std::cos(theta) - y * std::sin(theta))* std::sin(psy) + z * std::cos(psy);
             graph.append(sf::Vertex(sf::Vector2f(x_scale(x_final, size) , y_scale(y_final, size)), transparentRed));
         }
         window.draw(graph);
@@ -53,18 +60,40 @@ void origin_function_one(sf::RenderWindow& window, sf::VertexArray& graph, doubl
         graph.clear();
     }
 }
-//            double x_transformed = x_scale(x, size);
-//            double y_transformed = x_scale(y, size);
-//            double z_transformed = y_scale(z, size);
 
-//            double x_final = x * (pow(std::sin(psy),2)-1)  * std::cos(theta) * std::sin(theta) +
-//                             y * (pow(std::sin(psy) * std::sin(theta) , 2) + pow(std::cos(theta),2)) +
-//                             z * (-std::sin(psy) * std::cos(psy) * std::sin(theta));
-//
-//            double y_final = x * -std::cos(psy) * std::sin(theta) * std::cos(theta) +
-//                             y * -std::sin(psy)*cos(psy)*sin(theta) +
-//                             z * pow(std::cos(psy),2);
+void origin_function_two(sf::RenderWindow& window, sf::VertexArray& graph, double size, double two_variable_input_function(double, double), double start, double end, double x_angle, double y_angle) {
+    sf::Color color = sf::Color(128, 128, 128, 180);
+    double psy = pi * y_angle / 120;
+    double theta = pi * x_angle / 120;
+    double length = 0.2;
 
+    for (double x = start; x <= end; x += length) {
+        for (double y = start; y <= end; y += length) {
+            double z = two_variable_input_function(x, y);
+
+            // Adjust the transformed coordinates to fit with the axes
+            double x_final = x * std::sin(theta) + y * std::cos(theta);
+            double y_final = (x * std::cos(theta) - y * std::sin(theta))* std::sin(psy) + z * std::cos(psy);
+            graph.append(sf::Vertex(sf::Vector2f(x_scale(x_final, size) , y_scale(y_final, size)), color));
+        }
+        window.draw(graph);
+        graph.clear();
+    }
+//    axe1.append(sf::Vertex(sf::Vector2f(400 * std::sin(theta) , -400 * (std::sin(psy)) * (std::cos(theta))), sf::Color::Red));
+
+    for (double y = start; y <= end; y += length) {
+        for (double x = start; x <= end; x += length) {
+            double z = two_variable_input_function(x, y);
+
+            // Adjust the transformed coordinates to fit with the axes
+            double x_final = x * std::sin(theta) + y * std::cos(theta);
+            double y_final = z * std::cos(psy) + (x * std::cos(theta) - y * std::sin(theta))* std::sin(psy);
+            graph.append(sf::Vertex(sf::Vector2f(x_scale(x_final, size) , y_scale(y_final, size)), color));
+        }
+        window.draw(graph);
+        graph.clear();
+    }
+}
 
 
 #endif //INC_3DGRAPH_FUNCTION_H
--- main.cpp
+++ main.cpp
@@ -13,7 +13,9 @@ int main() {
 
     sf::RenderWindow window(sf::VideoMode(width, height), "3D Graph");
 //    sf::VertexArray EulerGraph(sf::LineStrip);
-    sf::VertexArray origin(sf::LineStrip);
+    sf::VertexArray one(sf::LineStrip);
+    sf::VertexArray two(sf::LineStrip);
+
 //    sf::VertexArray parameter(sf::LineStrip);
 //    sf::VertexArray parameter_point(sf::LineStrip);
 
@@ -43,6 +45,7 @@ int main() {
     double y_angle = 5;
     const double pi = 3.14159265358979;
     bool complex_type = true;
+    bool real_graph_on = false;
 
     sf::Clock clock;
     sf::Clock clock_t;
@@ -83,36 +86,22 @@ int main() {
 //                    case sf::Keyboard::T:
 //                        tracer = !tracer;  // Toggle tracer when 'T' is pressed
 //                        break;
-                    case sf::Keyboard::R:
-                        window.display();
-                        window.clear();
                     case sf::Keyboard::W:
                             size += 5;
                         break;
                     case sf::Keyboard::S:
                             size -= 5;
                         break;
-
-//                    case sf::Keyboard::Up:
-//                        if (!tracer)
-//                            move_UD += 1;
-//                        break;
-//                    case sf::Keyboard::Down:
-//                        if (!tracer)
-//                            move_UD -= 1;
-//                        break;
-//                    case sf::Keyboard::Right:
-//                        if (!tracer)
-//                            move_RL += 1;
-//                        break;
-//                    case sf::Keyboard::Left:
-//                        if (!tracer)
-//                            move_RL -= 1;
-//                        break;
+                    case sf::Keyboard::O:
+                        if (real_graph_on == true)
+                            real_graph_on = false;
+                        else
+                            real_graph_on = true;
+                        break;
                     default:
                         break;
                 }
-
+                size = (size < 5) ? 5 : ((size > 150.0) ? 150.0 : size);
             }
 
 
@@ -159,71 +148,65 @@ int main() {
 
         window.clear(sf::Color::White);
 
-        origin_function_one(window, origin, size, one_variable_function, -10, 10, x_angle, y_angle, complex_type);
-//
-//        sf::VertexArray axes(sf::Lines);
-//
-//        axes.append(sf::Vertex(sf::Vector2f(-(600.0f - graphView.getCenter().x), 0.0f), sf::Color::Black));
-//        axes.append(sf::Vertex(sf::Vector2f(600.0f + graphView.getCenter().x, 0.0f), sf::Color::Black));
-//        axes.append(sf::Vertex(sf::Vector2f(0.0f, -400.0f + graphView.getCenter().y), sf::Color::Black));
-//        axes.append(sf::Vertex(sf::Vector2f(0.0f, 400.0f + graphView.getCenter().y), sf::Color::Black));
-//
-////        axes.append(sf::Vertex(sf::Vector2f(131.0f, std::sin(pi) * (400.0f + graphView.getCenter().y)), sf::Color::Black));
-//
-//        double neo_x = abs(size * (std::cos(pi * x_angle / 50)));
-//        double neo_y = abs(size * (std::cos(pi * y_angle / 50)));
-//
-//
-//        if (neo_x > 0.01)
-//            for (double i = 0; i <= x_end; i += neo_x) {
-//                axes.append(sf::Vertex(sf::Vector2f( i, -3), sf::Color::Black));
-//                axes.append(sf::Vertex(sf::Vector2f(i, 3), sf::Color::Black));
-//            }
-//
-//
-//        if (neo_y > 0.01)
-//            for (double i = 0; i >= -y_end; i -= neo_y) {
-//                axes.append(sf::Vertex(sf::Vector2f(-3,i), sf::Color::Black));
-//                axes.append(sf::Vertex(sf::Vector2f(3,i), sf::Color::Black));
-//            }
-//
-//        window.draw(axes); 2
+        origin_function_one(window, one, size, one_variable_function_complex, -10, 10, x_angle, y_angle, complex_type);
+        if (real_graph_on == true)
+            origin_function_two(window, two, size, two_variable_real_function, -10, 10, x_angle, y_angle);
 
         double psy = pi * y_angle / 120;
         double theta = pi * x_angle / 120;
+        int length_axes = 30 * size;
 
         sf::VertexArray axe1(sf::Lines);
         axe1.append(sf::Vertex(sf::Vector2f(0.0f, 0.0f), sf::Color::Red));
-        axe1.append(sf::Vertex(sf::Vector2f(400 * std::sin(theta) , -400 * (std::sin(psy)) * (std::cos(theta))), sf::Color::Red));
+        axe1.append(sf::Vertex(sf::Vector2f(length_axes * std::sin(theta) , -length_axes * (std::sin(psy)) * (std::cos(theta))), sf::Color::Red));
 
         sf::VertexArray axe2(sf::Lines);
         axe2.append(sf::Vertex(sf::Vector2f(0.0f, 0.0f), sf::Color::Black));
-        axe2.append(sf::Vertex(sf::Vector2f(400 * std::cos(theta) , -400 * (std::sin(psy)) * (-std::sin(theta))), sf::Color::Black));
+        axe2.append(sf::Vertex(sf::Vector2f(length_axes * std::cos(theta) , -length_axes * (std::sin(psy)) * (-std::sin(theta))), sf::Color::Black));
 
         sf::VertexArray axe3(sf::Lines);
         axe3.append(sf::Vertex(sf::Vector2f(0.0f, 0.0f), sf::Color::Black));
-        axe3.append(sf::Vertex(sf::Vector2f(-400 * std::sin(theta), 400 * (std::sin(psy)) * (std::cos(theta))), sf::Color::Black));
+        axe3.append(sf::Vertex(sf::Vector2f(-length_axes * std::sin(theta), length_axes * (std::sin(psy)) * (std::cos(theta))), sf::Color::Black));
 
         sf::VertexArray axe4(sf::Lines);
         axe4.append(sf::Vertex(sf::Vector2f(0.0f, 0.0f), sf::Color::Black));
-        axe4.append(sf::Vertex(sf::Vector2f(-400 * std::cos(theta) , 400 * (std::sin(psy)) * (-std::sin(theta))), sf::Color::Black));
+        axe4.append(sf::Vertex(sf::Vector2f(-length_axes * std::cos(theta) , length_axes * (std::sin(psy)) * (-std::sin(theta))), sf::Color::Black));
 
         sf::VertexArray axe5(sf::Lines);
         axe5.append(sf::Vertex(sf::Vector2f(0.0f, 0.0f), sf::Color::Black));
-        axe5.append(sf::Vertex(sf::Vector2f(0.0f, 400 * (std::cos(psy))), sf::Color::Black));
+        axe5.append(sf::Vertex(sf::Vector2f(0.0f, length_axes * (std::cos(psy))), sf::Color::Black));
 
         sf::VertexArray axe6(sf::Lines);
         axe6.append(sf::Vertex(sf::Vector2f(0.0f, 0.0f), sf::Color::Red));
-        axe6.append(sf::Vertex(sf::Vector2f(0.0f, -400 * (std::cos(psy))), sf::Color::Red));
+        axe6.append(sf::Vertex(sf::Vector2f(0.0f, -length_axes * (std::cos(psy))), sf::Color::Red));
+
+        sf::VertexArray axe_x_dot(sf::Lines);
+        for (double i = -30* size; i <= 30 * size; i += 1 * size ) {
+            axe_x_dot.append(sf::Vertex(sf::Vector2f(i * std::sin(theta), 10 - i * (std::sin(psy)) * (std::cos(theta))), sf::Color::Black));
+            axe_x_dot.append(sf::Vertex(sf::Vector2f(i * std::sin(theta), -10 - i * (std::sin(psy)) * (std::cos(theta))), sf::Color::Black));
+        }
+
+        sf::VertexArray axe_y_dot(sf::Lines);
+        for (double i = -30* size; i <= 30 * size; i += 1 * size ) {
+            axe_y_dot.append(sf::Vertex(sf::Vector2f(i * std::cos(theta), 10 + i * (std::sin(psy)) * (std::sin(theta))), sf::Color::Black));
+            axe_y_dot.append(sf::Vertex(sf::Vector2f(i * std::cos(theta), -10 + i * (std::sin(psy)) * (std::sin(theta))), sf::Color::Black));
+        }
+
+        sf::VertexArray axe_z_dot(sf::Lines);
+        for (double i = -30 * size; i <= 30 * size; i += 1 * size ) {
+            axe_y_dot.append(sf::Vertex(sf::Vector2f(-10, i * std::cos(psy)), sf::Color::Black));
+            axe_y_dot.append(sf::Vertex(sf::Vector2f(10, i * std::cos(psy)), sf::Color::Black));
+        }
+
 
         window.draw(axe1);
         window.draw(axe2);
         window.draw(axe3);
         window.draw(axe4);
-
         window.draw(axe5);
         window.draw(axe6);
-
+        window.draw(axe_x_dot);
+        window.draw(axe_y_dot);
 
         // Display the contents of the window
         window.display();
